06e9a6ca1b474d8420e5a7c0757790a3390c14fa
JoelAShepherd <joel125shepherd@gmail.com>
2021-05-14 16:01:21 +0100

    README added, obsolete files removed, comments to main file added

---
+++ README.md
@@ -0,0 +1,11 @@
+# Rubik's Cube combinations solver
+This program can be used for calculating the number of different combinations that a Rubik's cube can make. 
+
+## Use
+Run the file in Node. Input the size of cube you wish to calculate on the command line as prompted.
+
+## Contributions
+Pull requests gladly recieved. I've no way of verifying if the large results are accurate to the true value, so any ideas or ways to check would be very much appreciated. 
+
+## Sources
+I used the maths from the Rubik's cube articles on [Wikipedia](https://en.wikipedia.org/wiki/Rubik%27s_Cube) as the basis for these calculations. 
--- generalSolv.js
+++ generalSolv.js
@@ -1,15 +1,19 @@
-
+//Factorial function for BigInts
 const bigFact = num => {
     return (num !== 1n) ? num * bigFact(num-1n) : 1n
 }
 
+//Count the size of the number of combinations
 const digits = num => {
     let bigString = num.toString();
     return bigString.length
 };
 
-let corners = bigFact(8n) * (3n**7n);
-let centerEdges = bigFact(12n) * (2n**10n);
+
+let corners = bigFact(8n) * (3n**7n);   //contribution of corner pieces to number of combinations
+
+let centerEdges = bigFact(12n) * (2n**10n); //contribution of center-edge pieces, 
+//for odd number cubes only
 
 let bigSize, edgeLength, centerSize, numOfInnerSets, numOfEdgeSets, odd;
 
@@ -19,15 +23,19 @@ const calculateDimensions = rubiksSize => {
     } else {
         odd = true;
     }
-    bigSize = BigInt(rubiksSize)
-    edgeLength = bigSize -2n;
-    centerSize = edgeLength ** 2n;
+    bigSize = BigInt(rubiksSize) //rubik's size as BigInt
+    edgeLength = bigSize -2n; //size of the edge without the corners
+    centerSize = edgeLength ** 2n;  //size of the center area for each face (no edges or corners)
     if (!odd){
         numOfInnerSets = centerSize / 4n;
+        //The centerSize is split into 4 groups of pieces.
         numOfEdgeSets = edgeLength / 2n;
+        //Edges are split into sets equal to half the non-corner edge-length.
     } else {
         numOfInnerSets = (centerSize - 1n)/ 4n;
+        //-1n for the center piece that remains static
         numOfEdgeSets = (edgeLength -1n) / 2n;
+        //-1n for the center edge pieces (odd cubes only)
     }
     console.log(`Rubiks Size: ${rubiksSize}    Edge Length: ${edgeLength}`)
     console.log(`Center Size: ${centerSize}    Number of Inner Sets: ${numOfInnerSets}`);
@@ -39,10 +47,12 @@ let reducer, innerSetMultiplier, edgeSetMultiplier, combinations;
 const rubiksSolver = r => {
     console.log()
     calculateDimensions(r);
-    edgeSetMultiplier = (bigFact(24n))**numOfEdgeSets
+    edgeSetMultiplier = (bigFact(24n))**numOfEdgeSets //contributions of edge sets to the combinations
     if (odd) {
-        reducer = (24n**6n)**numOfInnerSets 
+        reducer = (24n**6n)**numOfInnerSets
+        //reducer to negate indestinguishable permutations 
         innerSetMultiplier = (bigFact(24n))**numOfInnerSets;
+        //contribution of the inner sets
         combinations = (corners * centerEdges * innerSetMultiplier * edgeSetMultiplier) / reducer
         
     } else {
@@ -58,9 +68,9 @@ const rubiksSolver = r => {
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
-  })
+})
   
   readline.question(`Please enter the size of Cube you wish to calculate: `, num => {
     rubiksSolver(parseInt(num));
     readline.close();
-  })
+})
